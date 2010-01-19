01d21754ab2b49ee5260ef7621d4515a66425ad4
RC Howe <rchowe1@gmail.com>
2010-01-19 15:55:56 -0500

    Added documentation for the kicker.

--- Kicker.h
+++ Kicker.h
@@ -12,11 +12,27 @@
 class Kicker
 {
 public:
+	/**
+	 *	Creates a new kicker.
+	 *
+	 *	@param motorChannel Channel for the motor that winds the kicker
+	 *	@param pneumaticChannel The pneumatic that holds the kicker back
+	 *	@param encoderChannel1 The encoder
+	 *	@param encoderChannel2 The encoder
+	 */
 	explicit Kicker( UINT32 motorChannel, UINT32 pneumaticChannel,
 					 UINT32 encoderChannel1, UINT32 encoderChannel2 );
+	
+	/**
+	 *	Destroys the encoder
+	 */
 	virtual ~Kicker();
 	
-	void Kick();
+	/**
+	 *	Winds up and kicks with a certain power.
+	 *	This will be replaced with a kick-then-wind system.
+	 */
+	void Kick( float power );
 	
 private:
 	Encoder encoder;
